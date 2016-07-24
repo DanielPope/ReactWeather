efce0b0bc8c5b112ee25cd556feeccc517d2395d
Daniel Pope <dpope@dminc.com>
2016-07-24 19:54:46 -0400

    Styled the about page

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -13,12 +13,12 @@ var About = (props) => {
         Here are some of the tools I used:
         <ul>
           <li>
-            <a href="http://facebook.github.io/react">  - This was the JS
-              framework used.</a>
+            <a href="http://facebook.github.io/react">React</a>  - This was the JS
+              framework used.
           </li>
           <li>
-            <a href="http://openweathermap.org"> - I used open weather map
-              to search frot he weather data by the city name</a>
+            <a href="http://openweathermap.org">Open Weather Mape</a> - I used open
+              weather map to search frot he weather data by the city name
           </li>
         </ul>
       </p>
