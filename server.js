a91793d0b91ad4efbc8ad52a264797921ba9e009
Daniel Pope <dpope@dminc.com>
2016-07-24 18:46:21 -0400

    update middleware for localhost

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ var app = express();
 const PORT  = process.env.PORT || 3000;
 
 app.use(function(req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'http'){
-    next();
-  } else {
+  if (req.headers['x-forwarded-proto'] === 'https'){
     res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
   }
 });
 
@@ -16,4 +16,3 @@ app.use(express.static('public'));
 app.listen(PORT,function (){
   console.log('Express server is up on port' + PORT)
 });
- 
