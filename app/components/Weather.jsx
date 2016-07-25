0295822b92b43e24896a777aff721546c8d4e99e
Daniel Pope <dpope@dminc.com>
2016-07-25 00:08:49 -0400

    fixed error modal

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -9,14 +9,19 @@ var openWeatherMap = require('openWeatherMap');
 var Weather = React.createClass ({
   getInitialState: function () {
     return {
-      isLoading: false,
-      errorMessage: undefined
+      isLoading: false
       }
   },
 
   handleSearch : function (location) {
     var that = this;
-    this.setState({isLoading: true});
+
+    this.setState({
+      isLoading: true,
+      errorMessage: undefined,
+      location: undefined,
+      temp: undefined
+    });
 
     openWeatherMap.getTemp(location).then(function(temp) {
       that.setState({
@@ -31,11 +36,26 @@ var Weather = React.createClass ({
       });
     });
   },
+  componentDidMount: function () {
+    var location = this.props.location.query.location;
 
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = "#/";
+    }
+  },
+  componentWillReceiveProps: function (newProps) {
+    var location = newProps.location.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = "#/";
+    }
+  },
   render: function () {
     var {isLoading, temp, location, errorMessage} = this.state;
 
-    function renderMessage() {
+    function renderMessage () {
       if (isLoading) {
         return <h3 className="text-center">Fetching Weather...</h3>;
       } else if (temp && location) {
